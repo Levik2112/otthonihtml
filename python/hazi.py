5a2501c4514fdbd74103e614248254a6d4189b80
Levik2112 <dobi.levente.domonkos.21@ady-nagyatad.hu>
2022-09-11 15:49:26 +0200

    sf

---
+++ python/hazi.py
@@ -0,0 +1,24 @@
+print("Dobi Levente")
+print("2022.09.11")
+print("      __")
+print("    - ||")
+print("   / \||")
+print("  /   \|")
+print(" /     \ ")
+print("|       |")
+print("|       |")
+print("---------")
+
+
+
+print(" _______________")
+print("|               |")
+print("|  C:\> |       |")
+print("|               |")
+print("|               |")
+print("|               |")
+print("|               |")
+print("|               |")
+print("|_______________|")
+
+
