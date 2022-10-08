59fd8ed29f0a9d236019e312c44ea4616c8691d8
levente <dobi.levente.domonkos.21@ady-nagyatad.hu>
2022-10-08 21:04:37 +0200

    asd

---
+++ python/projekt.py
@@ -0,0 +1,21 @@
+a=int(input("Adj meg egy szamot: "))
+b=int(input("Adj meg  meg egy szamot: "))
+c=input("Adj meg egy muveleti jelet: ")
+
+
+if c=="+":
+    print("Az eredmeny =",+ a+b)
+elif c=="-":
+    print("Az eredmeny =",+a-b)
+elif c=="*":
+    print("Az eredmeny =",+a*b)
+elif c=="/":
+    print("Az eredmeny =",+a/b)
+elif c=="**":
+    print("Az eredmeny =",+a**b)
+elif c=="%":
+    print("Az eredmeny =",+a%b)
+elif c=="//":
+    print("Az eredmeny =",+a//b)
+
+
