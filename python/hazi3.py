08a8206197269756cb22675e98cbabb68cf6f907
Levik2112 <98027590+Levik2112@users.noreply.github.com>
2022-12-08 09:48:39 +0100

    Update hazi3.py

--- python/hazi3.py
+++ python/hazi3.py
@@ -3,5 +3,5 @@ import random
 
 i=0
 while i!=1000:
-    print(random.randint(2000,2099),".",random.randint(1,12),".",random.randint(1,31))
+    print(random.randint(1900,1999),".",random.randint(1,12),".",random.randint(1,31))
     i+=1
